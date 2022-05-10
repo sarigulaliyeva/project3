b4d4e54c39d53fe82d9019c306e05167637ef969
sarigulaliyeva <100438856+sarigulaliyeva@users.noreply.github.com>
2022-05-11 01:07:25 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let leftBtn=document.querySelectorAll('.leftBtn button');
 let rightBtn=document.querySelectorAll('.rightBtn button');
 let leftValyuta = "RUB";
 let rightValyuta = "USD";
-
+let rightVal=document.querySelector('.rightValyuta');
+let leftVal=document.querySelector('.leftValyuta')
 leftBtn.forEach((item)=>{
     item.addEventListener('click', function(){
         leftBtn.forEach((item)=>{
@@ -55,7 +56,16 @@ function fetchRight(left, right){
         return res.json();
     })
     .then((data)=>{
-        rightInput.value=Number(leftInput.value)*data.rates[`${right}`]
+        rightInput.value=leftInput.value*data.rates[`${right}`];
+
+        leftVal.innerHTML=`1${data.leftValyuta}=${data.rates[`${right}`]}${right}`;
+        fetch(`https://api.exchangerate.host/latest?base=${right}&symbols=${left}`)
+        .then((res)=>{
+            res.json();
+        })
+        .then((data)=>{
+            rightVal.innerHTML=`${data.leftValyuta}=${data.rates[`${left}`]}${left}`;
+        })
     })
 }
 function fetchLeft(left, right){
@@ -64,6 +74,15 @@ function fetchLeft(left, right){
         return res.json();
     })
     .then((data)=>{
-        leftInput.value=rightInput.value*data.rates[`${right}`]
+        leftInput.value=rightInput.value*data.rates[`${right}`];
+        
+        rightVal.innerHTML = `1${data.leftValyuta}=${data.rates[`${left}`]}${left}`;
+        fetch(`https://api.exchangerate.host/latest?base=${right}&symbols=${left}`)
+        .then((res)=>{
+            return res.json();
+        })
+        .then((data)=>{
+            leftVal.innerHTML=`1${data.leftValyuta}=${data.rates[`${right}`]}${right}`
+        })
     })
 }
