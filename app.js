8ee741bfff9f1b720bcfaaae62933c91050718ca
sarigulaliyeva <100438856+sarigulaliyeva@users.noreply.github.com>
2022-05-10 17:04:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 let btn=document.querySelectorAll('.enter1 button');
 let leftInput=document.querySelector('.leftInput');
 let rightInput=document.querySelector('.rightInput');
-btn.forEach((item)=>{
-    item.addEventListener('click',()=>{
-        btn.forEach((item)=>{
-            item.classList.remove('active');
-        })
-    })
-    item.addEventListener('click',()=>{
-        item.classList.add('active');
-    })
-})
+let leftBtn=document.querySelectorAll('.leftBtn button');
+let rightBtn=document.querySelectorAll('.rightBtn button');
 let leftValyuta = "RUB";
 let rightValyuta = "USD";
+
+leftBtn.forEach((item)=>{
+    item.addEventListener('click', function(){
+        leftBtn.forEach((item)=>{
+            item.classList.remove('active');
+        });
+        leftValyuta = this.innerHTML;
+        Api(this.parentElement.classList[0]);
+    });
+    item.addEventListener('click', ()=>{
+        item.classList.add('active')
+    });
+});
+
+rightBtn.forEach((item)=>{
+    item.addEventListener('click', function(){
+        rightBtn.forEach((item)=>{
+            item.classList.remove('active');
+        });
+        rightValyuta = this.innerHTML;
+        Api(this.parentElement.classList[0]);
+    });
+    item.addEventListener('click', ()=>{
+        item.classList.add('active');
+    });
+});
+
+function Api(parentBtn){
+    if(parentBtn == 'rightBtn'){
+        fetchRight(leftValyuta, rightValyuta);
+    }
+    if(parentBtn == 'leftBtn'){
+        fetchLeft(leftValyuta, rightValyuta);
+    }
+}
 function changeInput(){
     leftInput.addEventListener('input', ()=>{
         fetchRight(leftValyuta,rightValyuta)
@@ -28,7 +55,7 @@ function fetchRight(left, right){
         return res.json();
     })
     .then((data)=>{
-        rightInput.value=leftInput.value*data.rates[`${right}`]
+        rightInput.value=Number(leftInput.value)*data.rates[`${right}`]
     })
 }
 function fetchLeft(left, right){
